621499d3fe81bb604e3f935974b0b3ece9ecc37a
Festus Maithya <festusmaithya264@gmail.com>
2023-04-19 22:27:33 +0300

    Update convert_numbers.c

--- convert_numbers.c
+++ convert_numbers.c
@@ -3,7 +3,7 @@
  * Auth: Festus Maithya
  */
 
-#include "holberton.h"
+#include "main.h"
 
 unsigned int convert_di(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
