a85587e877d18e01c0ec312b361251f45372a178
Festus Maithya <festusmaithya264@gmail.com>
2023-04-19 22:27:04 +0300

    Update convert_strings.c

--- convert_strings.c
+++ convert_strings.c
@@ -3,7 +3,7 @@
  * Auth: Festus Maithya
  */
 
-#include "holberton.h"
+#include "main.h"
 
 unsigned int convert_s(va_list args, buffer_t *output,
 		unsigned char flags, int wid, int prec, unsigned char len);
